fb77faee282d42f7667bd104c21c71a10cf4c594
Zhexin Wu <1025839251@qq.com>
2021-05-14 11:06:34 +0800

    K >> d

--- oblique.py
+++ oblique.py
@@ -117,7 +117,7 @@ def make_lr_weight_decay(lam_W=0.3, lam_H=0.3, lam_b=0.3):
         W, H, b = X
         num_cls = b.shape[0]
         assert H.shape[-1] % num_cls == 0, "number of samples must be a multiple of number of classes"
-        assert W.shape[0] >= W.shape[-1], "number of features must be no less than number of training samples"
+        # assert W.shape[0] >= W.shape[-1], "number of features must be no less than number of training samples"
         N = H.shape[-1] // num_cls  # number of training samples per class
         cost = 0.5 * (lam_W * np.linalg.norm(W) ** 2 + lam_H * np.linalg.norm(H) ** 2 + lam_b * np.linalg.norm(b) ** 2)
         cls_labels = np.kron(np.arange(num_cls), np.ones((N,))).astype(np.int64)
@@ -266,39 +266,66 @@ if __name__ == '__main__':
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot
 
-    # MCR^2, interactive 3D plot
-    num_cls = 2  # 3 or 2
-    sample_per_cls = 10
-    N = sample_per_cls * num_cls
+    # # MCR^2, interactive 3D plot
+    # num_cls = 2  # 3 or 2
+    # sample_per_cls = 10
+    # N = sample_per_cls * num_cls
+    # M = 3
+    # solver = TrustRegions()
+    # # solver = SteepestDescent()
+    # manifold = Oblique(M, N)
+    # prob = Problem(manifold=manifold, cost=make_mcr2_loss(num_cls))
+    # with open("loss.txt", "w") as wf:
+    #     with redirect_stdout(wf):
+    #         Xopt = solver.solve(prob)
+    #
+    # data = convert_stdout_to_data("loss.txt", "RTR")
+    #
+    # fig, axes = plt.subplots(1, 2)
+    # axes[0].plot(data[:, 0])
+    # axes[0].set_title("fval")
+    # axes[1].plot(data[:, 1])
+    # axes[1].set_title("norm of grad")
+    #
+    # # Xopt *= numpy.sign(Xopt[0, :])  # get rid of sign flip, but better not to do it to see obvious orthogonality
+    # fig = plt.figure(figsize=(8, 8))
+    # axis = fig.gca(projection="3d")
+    # colors = ["r", "b", "g"]
+    # for cls in range(num_cls):
+    #     label = cls + 1
+    #     start = cls * sample_per_cls
+    #     end = (cls + 1) * sample_per_cls
+    #     axis.scatter(*[Xopt[i, start: end] for i in range(Xopt.shape[0])], color=colors[cls], label=f"cls {label}")
+    # axis.grid(True)
+    # axis.set_xlim([-1, 1])
+    # axis.set_ylim([-1, 1])
+    # axis.set_zlim([-1, 1])
+    # axis.legend()
+    # plt.show()  # You should be able to see subspaces are orthogonal to each other
+
+    # ETF for K >> d, 3D
     M = 3
-    solver = TrustRegions()
-    # solver = SteepestDescent()
-    manifold = Oblique(M, N)
-    prob = Problem(manifold=manifold, cost=make_mcr2_loss(num_cls))
+    K = 7
+    samples_per_cls = 5
+    solver = SteepestDescent()
+    manifold = Product((Oblique(M, K), Oblique(M, samples_per_cls * K), Euclidean(K)))
+    prob = Problem(manifold=manifold, cost=make_lr_weight_decay(0, 0, 0.01))
+
     with open("loss.txt", "w") as wf:
         with redirect_stdout(wf):
             Xopt = solver.solve(prob)
 
-    data = convert_stdout_to_data("loss.txt", "RTR")
-
+    data = convert_stdout_to_data("loss.txt")
     fig, axes = plt.subplots(1, 2)
     axes[0].plot(data[:, 0])
-    axes[0].set_title("fval")
     axes[1].plot(data[:, 1])
-    axes[1].set_title("norm of grad")
 
-    # Xopt *= numpy.sign(Xopt[0, :])  # get rid of sign flip, but better not to do it to see obvious orthogonality
-    fig = plt.figure(figsize=(8, 8))
+    W, H, b = Xopt
+    fig = plt.figure(figsize=(6, 6))
     axis = fig.gca(projection="3d")
-    colors = ["r", "b", "g"]
-    for cls in range(num_cls):
-        label = cls + 1
-        start = cls * sample_per_cls
-        end = (cls + 1) * sample_per_cls
-        axis.scatter(*[Xopt[i, start: end] for i in range(Xopt.shape[0])], color=colors[cls], label=f"cls {label}")
     axis.grid(True)
+    axis.scatter(W[0, :], W[1, :], W[2, :])
     axis.set_xlim([-1, 1])
     axis.set_ylim([-1, 1])
     axis.set_zlim([-1, 1])
-    axis.legend()
-    plt.show()  # You should be able to see subspaces are orthogonal to each other
+    plt.show()
